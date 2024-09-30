b7e245339f60d2d4271bebfd13495b991bbaa418
Apurva Aggarwal <72258408+ProNoob0510@users.noreply.github.com>
2024-09-30 17:59:15 -0400

    Week2 code added

---
+++ week2.py
@@ -0,0 +1,15 @@
+import numpy as np 
+
+class Neuron:
+    def __init__(self, num_inputs):
+        self.num_inputs = num_inputs
+        self.weights = np.random.randn(num_inputs)
+        self.bias = np.random.randn()
+        self.output = 0
+
+
+    def forward(self, inputs):
+        # Calculate the dot product and add the bias
+        self.output = np.dot(self.weights, inputs) + self.bias
+        return self.output
+    
