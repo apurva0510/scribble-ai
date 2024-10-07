9367d04837be7b97050432629a1ce530b0e90041
Apurva Aggarwal <72258408+ProNoob0510@users.noreply.github.com>
2024-10-07 18:27:04 -0400

    week4 further

--- classes.py
+++ classes.py
@@ -1,4 +1,4 @@
-import numpy as np 
+import numpy as np
 
 def relu(x):
     return np.maximum(0, x)
@@ -6,7 +6,6 @@ def relu(x):
 def softmax(x):
     exp_x = np.exp(x - np.max(x))  # Subtract max(x) for numerical stability
     return exp_x / exp_x.sum(axis=0)
-
 
 class Neuron:
     def __init__(self, num_inputs, activation_function=relu):
@@ -16,70 +15,52 @@ class Neuron:
         self.activation_function = activation_function
         self.output = 0
 
-
     def forward(self, inputs):
         # Calculate the dot product and add the bias
         self.output = np.dot(self.weights, inputs) + self.bias
-        # Apply the ReLU activation function
-        self.output = relu(self.output)
+        # Apply the activation function
+        self.output = self.activation_function(self.output)
         return self.output
 
-
 class Layer:
-
-    def init(self, num_inputs, num_neurons, activation_function=relu):
+    def __init__(self, num_inputs, num_neurons, activation_function=relu):
         self.num_inputs = num_inputs
         self.num_neurons = num_neurons
         # Creating the neurons
-        # Here, create the number of neurons required and store them in a list
-        self.neurons = [Neuron(num_inputs) for _ in range(num_neurons)]
+        self.neurons = [Neuron(num_inputs, activation_function) for _ in range(num_neurons)]
         self.activation_function = activation_function
         self.outputs = []
 
     def forward(self, inputs):
-        # Take the inputs and pass them each neuron's forward functions and store the outputs in the self.outputs list
+        # Take the inputs and pass them to each neuron's forward function and store the outputs in the self.outputs list
         self.outputs = [neuron.forward(inputs) for neuron in self.neurons]
-
+        # Apply the activation function to the entire layer's outputs if it's softmax
+        self.outputs = self.activation_function(np.array(self.outputs))
+        self.outputs = softmax(self.outputs)
+        return self.outputs
 
 class NeuralNetwork:
-
-    def init(self, num_inputs, num_hidden_layers, num_hidden_layer_neurons, num_outputs):
+    def __init__(self, num_inputs, num_hidden_layers, num_hidden_layer_neurons, num_outputs):
         self.num_inputs = num_inputs
         self.num_hidden_layers = num_hidden_layers
         self.num_hidden_layer_neurons = num_hidden_layer_neurons
         self.num_outputs = num_outputs
 
-        # Now that we have all the required variables, go ahead and create the layers
-
-        # Always remember that we do NOT need to create a layer for the inputs. The initial
-        # inputs that we get make up the first input layer. So, we start from the first hidden
-        # layer and create layers all the way up to the last (output) layer
-
+        # Create the layers
         self.layers = []
 
-        # Create the hidden layers
+        # Create the hidden layers with relu activation function
         for _ in range(num_hidden_layers):
-            self.layers.append(Layer(num_inputs, num_hidden_layer_neurons))
+            self.layers.append(Layer(num_inputs, num_hidden_layer_neurons, activation_function=relu))
             num_inputs = num_hidden_layer_neurons  # Update num_inputs for the next layer
 
-        # Create the output layer
-        self.layers.append(Layer(num_hidden_layer_neurons, num_outputs))
-
-        # Create the appropriate number of hidden layers each with the appropriate number of neurons
-        # Tip: Use a for loop to create the hidden layers
-
-        # At the end, create the output layer
-        output_layer = Layer(num_hidden_layer_neurons, num_outputs)
-
+        # Create the output layer with softmax activation function
+        self.layers.append(Layer(num_hidden_layer_neurons, num_outputs, activation_function=softmax))
 
     def forward(self, inputs):
         '''
         Take the inputs and pass those inputs to each layer in the network
-        Tip, use a for loop and one variable to keep track of the outputs of a single layer
-        Keep updating that single variable with the outputs of the layers
-        At the end, whatever is in that variable will be the output of the last layer
         '''
-
         for layer in self.layers:
             layer.forward(inputs)
             inputs = layer.outputs
